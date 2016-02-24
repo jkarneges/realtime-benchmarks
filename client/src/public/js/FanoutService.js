5fc31ce95bff1f816c1efd8c53a528cc2b9c5fe8
Justin Karneges <justin@affinix.com>
2016-02-24 14:34:37 -0800

    update fanout service

--- client/src/public/js/FanoutService.js
+++ client/src/public/js/FanoutService.js
@@ -8,24 +8,30 @@ function FanoutService( channelName, listener ) {
   this.name = 'Fanout';
   this.realmId = 'd7d63e3a';
 
-  this.client = new Fpp.Client('http://pubsub.fanout.io/r/' + this.realmId);
+  this.client = new Faye.Client('http://' + this.realmId + '.fanoutcdn.com/bayeux');
 
   var self = this;
 
-  this.channel = this.client.Channel(this._channelName);
-  this.channel.on('state-changed', function (state) {
-    if (state.type == 'subscribed') {
-      self._onReady();
-    }
-  });
-  this.channel.on('data', function (data) {
+  this.subscription = this.client.subscribe('/' + this._channelName, function (data) {
     self._onMessage( data );
+  });
+  this.subscription.then(function () {
+    self._onReady();
   });
 }
 FanoutService.prototype = new BenchmarkService;
 
 FanoutService.prototype.send = function( data ) {
-  var body = { items: [ { fpp: data } ] };
+  var body = {
+    items: [
+      {
+        channel: this._channelName,
+        formats: {
+          'json-object': data
+        }
+      }
+    ]
+  };
   var xhr = new XMLHttpRequest();
   xhr.onreadystatechange = function() {
     if (xhr.readyState === 4) {
@@ -36,12 +42,11 @@ FanoutService.prototype.send = function( data ) {
   };
   xhr.open('POST', 'http://api.fanout.io/realm/' +
                     this.realmId +
-                    '/publish/' +
-                    this._channelName + '/', true);
+                    '/publish/', true);
   xhr.setRequestHeader('Content-Type', 'application/json');
   xhr.send(JSON.stringify(body));
 };
 
 FanoutService.prototype.disconnect = function( data ) {
-  this.channel.cancel();
+  this.subscription.cancel();
 };
